50143d008409c7e31c476c5ce14354a9a7a51614
Anubhav <gupta.anubhav25@gmail.com>
2019-06-09 22:55:37 +0530

    css fix

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import store from "./store";
 import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
+//import store from "./store";
+//import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("frame")
-);
+ReactDOM.render(<App />, document.getElementById("frame"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
